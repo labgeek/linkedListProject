c4784ebc047152195941e06c1397b94ddd4dd0c2
JD <labgeek@users.noreply.github.com>
2020-08-06 17:57:50 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include "List.h";
+#include <iostream>
+#include <ctime>   // for time()
+using namespace std;
+
+
+int main()
+{
+	List link;
+	link.addNode(3);
+	link.addNode(5);
+	link.addNode(7);
+	link.addNode(8);
+	link.addNode(9);
+
+	link.PrintList();
+	link.deleteNode(3);
+	link.PrintList();
+  return 0;
+}
